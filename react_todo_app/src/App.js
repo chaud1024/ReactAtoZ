8ce512c04764b160486a0a70fe3cb85e85902bf3
chaud1024 <chaud1024@gmail.com>
2022-07-23 18:03:09 +0900

    using filter to indicate which data I want to delete

--- react_todo_app/src/App.js
+++ react_todo_app/src/App.js
@@ -30,9 +30,18 @@ export default class App extends Component {
       id: "2",
       title: "청소하기",
       completed: false,
+    },
+    {
+      id: "3",
+      title: "노션세팅",
+      completed: false,
     }
   ];
 
+  handleClick = (id) => { // data.id to the parameter(which I clicked THAT id)
+    let newTodoData = this.todoData.filter(data => data.id !== id) // (id: THAT id what I clicked)
+    console.log('newTodoData', newTodoData);
+  }
 
   render() {
     return (
@@ -47,7 +56,7 @@ export default class App extends Component {
             <div style={this.getStyle()} key={data.id}>
               <input type="checkbox" defaultChecked={false} />
                 {data.title}
-              <button style={this.btnStyle}>X</button>
+              <button style={this.btnStyle} onClick={() => this.handleClick(data.id)}>X</button>
           </div>
           ))}
           
