dc7a5efb4788b3ec091cd0886aed822054aacd6e
chaud1024 <chaud1024@gmail.com>
2022-07-23 16:58:40 +0900

    put functional style in the jsx file

--- react_todo_app/src/App.js
+++ react_todo_app/src/App.js
@@ -12,6 +12,14 @@ export default class App extends Component {
     float : "right"
   }
 
+  getStyle = () => {
+   return {
+    padding : "10px",
+    borderBottom : "1px #ccc dotted",
+    TextDecoder : "none"
+   } 
+  }
+
 
   render() {
     return (
@@ -22,7 +30,7 @@ export default class App extends Component {
           <h1>할 일 목록</h1>
           </div>
           
-          <div>
+          <div style={this.getStyle()}>
             <input type="checkbox" defaultChecked={false} />
             공부하기
             <button style={this.btnStyle}>X</button>
