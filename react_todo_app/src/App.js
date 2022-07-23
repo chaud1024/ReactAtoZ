39ba0e96461b9a5dc5e679fecb19d608f2b34501
chaud1024 <chaud1024@gmail.com>
2022-07-23 16:56:38 +0900

    put style in the jsx file

--- react_todo_app/src/App.js
+++ react_todo_app/src/App.js
@@ -2,11 +2,32 @@ import React, { Component } from "react";
 import './App.css';
 
 export default class App extends Component {
+
+  btnStyle = {
+    color : "#fff",
+    border : "none",
+    padding : "5px 9px",
+    borderRadius : "50%",
+    cursor : "pointer",
+    float : "right"
+  }
+
+
   render() {
     return (
       <div className="container">
         <div className="todoBlock">
+
+          <div className="title">
           <h1>할 일 목록</h1>
+          </div>
+          
+          <div>
+            <input type="checkbox" defaultChecked={false} />
+            공부하기
+            <button style={this.btnStyle}>X</button>
+          </div>
+
         </div>
       </div>
     )
