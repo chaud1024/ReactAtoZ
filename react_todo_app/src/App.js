25bda150257a2013daea07ad6bc5a1cf952d50b8
chaud1024 <chaud1024@gmail.com>
2022-07-25 20:49:02 +0900

    Separate Lists component

--- react_todo_app/src/App.js
+++ react_todo_app/src/App.js
@@ -1,34 +1,13 @@
 import React, { useState } from "react";
 import './App.css';
+import Lists from "./components/Lists";
+
+
 
 export default function App() {
 
   const [todoData, setTodoData] = useState([]);
   const [value, setValue] = useState("");
-
-  const btnStyle = {
-    color : "#fff",
-    border : "none",
-    padding : "5px 9px",
-    borderRadius : "50%",
-    cursor : "pointer",
-    float : "right"
-  }
-
-  const getStyle = (completed) => {
-   return {
-    padding : "10px",
-    borderBottom : "1px #ccc dotted",
-    textDecoration : completed ? "line-through" : "none"
-   } 
-  }
-
-
-
-  const handleClick = (id) => { // data.id to the parameter(which I clicked THAT id)
-    let newTodoData = todoData.filter(data => data.id !== id); // (id: THAT id what I clicked)
-    setTodoData(newTodoData);
-  }
 
   const handleChange = (e) => { // get an event
     // console.log('e', e.target.value) 
@@ -50,16 +29,7 @@ export default function App() {
     setValue("");
   }
 
-  const handleCompleteChange = (id) => {
-    let newTodoData = todoData.map(data => {
-      if(data.id === id) {
-        data.completed = !data.completed;
-      }
-      return data;
-    })
-
-    setTodoData(newTodoData);
-  }
+  
 
     return (
       <div className="container">
@@ -69,13 +39,10 @@ export default function App() {
           <h1>할 일 목록</h1>
           </div>
 
-          {todoData.map((data) => (
-            <div style={getStyle(data.completed)} key={data.id}>
-              <input type="checkbox" defaultChecked={false} onChange={() => handleCompleteChange(data.id)} />
-                {data.title}
-              <button style={btnStyle} onClick={() => handleClick(data.id)}>X</button>
-          </div>
-          ))}
+          <Lists
+            todoData={todoData}
+            setTodoData={setTodoData}
+          />
           
           <form style={{ display : "flex" }} onSubmit={handleSubmit}>
             <input
