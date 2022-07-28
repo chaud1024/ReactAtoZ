38d85b6357eb042d91baca440611105396eb9541
chaud1024 <chaud1024@gmail.com>
2022-07-28 22:34:31 +0900

    Creating components

--- react_netflix_app/src/App.js
+++ react_netflix_app/src/App.js
@@ -1,7 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+  const key = process.env.REACT_APP_APIKEY
+  console.log(key)
+  useEffect(()=>{
+    console.log(key)
+  },[])
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +15,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+        { process.env.REACT_APP_APIKEY}
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
