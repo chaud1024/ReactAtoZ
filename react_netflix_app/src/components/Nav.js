c9a6499951a68dbd987e314c84653c1104aa35d9
chaud1024 <chaud1024@gmail.com>
2022-07-28 23:17:39 +0900

    Nav scrill animation

--- react_netflix_app/src/components/Nav.js
+++ react_netflix_app/src/components/Nav.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import './Nav.css';
 
+
 const Nav = () => {
+
+    const [show, setShow] = useState(false);
+
+    useEffect(() => {
+      window.addEventListener("scroll", () => {
+        // console.log('window.scrollY', window.scrollY)
+        if(window.scrollY > 50) {
+            setShow(true);
+        } else {
+            setShow(false);
+        }
+      })
+    
+      return () => {
+        window.removeEventListener("scroll", () => {});
+      }
+    }, []);
+    
+
   return (
-    <nav className='nav'>
+    <nav className={`nav ${show && "nav__black"}`}>
         <img
             src="./asset/img/netflix_logo.png" alt="Netflix logo"
             className='nav__logo'
