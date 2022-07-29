e7073a9edad2439320db6e09b7b8c1fc251f7cbb
chaud1024 <chaud1024@gmail.com>
2022-07-29 23:42:30 +0900

    route editied

--- react_netflix_app/src/App.js
+++ react_netflix_app/src/App.js
@@ -1,52 +1,37 @@
-import requests from "./api/requests";
-import Banner from "./components/Banner";
 import Nav from "./components/Nav";
-import Row from "./components/Row";
 import Footer from "./components/Footer";
+import { Outlet, Routes, Route } from "react-router-dom";
 
+import SearchPage from "./pages/SearchPage";
+import MainPage from "./pages/MainPage";
+import DetailPage from "./pages/DetailPage";
 
 import './App.css';
 
+const Layout = () => {
+  return(
+    <div>
+      <Nav />
+
+      <Outlet />
+
+      <Footer />
+    </div>
+  )
+}
 
 function App() {
 
   return (
     <div className="app">
-      <Nav />
-      <Banner />
+      <Routes>
+        <Route path="/" element={<Layout/>} >
+          <Route index element={<MainPage/>} />
+          <Route path=":movieId" element={<DetailPage/>} />
+          <Route path="search" element={<SearchPage/>} />
+        </Route>
+      </Routes>
 
-      <Row
-        title="NETFLIX ORIGINALS"
-        id="NO"
-        fetchUrl={requests.fetchNetflixOriginals}
-        isLargeRow
-      />
-
-      <Row
-        title="Trending Now"
-        id="TN"
-        fetchUrl={requests.fetchTrending}
-      />
-
-      <Row
-        title="Top Rated"
-        id="TR"
-        fetchUrl={requests.fetchTopRated}
-      />
-
-      <Row
-        title="Action Movies"
-        id="AM"
-        fetchUrl={requests.fetchActionMovies}
-      />
-
-      <Row
-        title="Comedy movies"
-        id="CM"
-        fetchUrl={requests.fetchComedyMovies}
-      />
-
-      <Footer />
     </div>
   );
 }
