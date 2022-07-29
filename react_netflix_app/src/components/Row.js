22dd775f39b46585209cd6826a139f5094c9e2cb
chaud1024 <chaud1024@gmail.com>
2022-07-29 17:36:08 +0900

    Row css and slider function

---
+++ react_netflix_app/src/components/Row.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react'
+import axios from '../api/axios'
+
+import './Row.css';
+
+const Row = ({ title, id, fetchUrl, isLargeRow }) => {
+
+    const [movies, SetMovies] = useState([]);
+
+    useEffect(() => {
+        fetchMovieData();
+
+    }, []);
+
+    const fetchMovieData = async () => {
+        const request = await axios.get(fetchUrl);
+        console.log('request', request);
+        SetMovies(request.data.results);
+    }
+
+    const BASE_URL = "https://image.tmdb.org/t/p/original/";
+    
+
+  return (
+    <section className="row">
+        <h2>{title}</h2>
+        <div className="slider">
+            <div className="slider__arrow-left">
+                <span className="arrow"
+                    onClick={() => {document.getElementById(id).scrollLeft -= window.innerWidth -80;}}
+                >
+                    {"<"}
+                </span>
+            </div>
+            <div id={id} className="row__posters">
+                {movies.map((movie) => (
+                    <img
+                        key={movie.id}
+                        className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                        src={`${BASE_URL}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+                        loading="lazy"
+                        alt={movie.name}
+                    />
+                ))}
+            </div>
+            <div className="slider__arrow-right">
+                <span className='arrow'
+                    onClick={() => {document.getElementById(id).scrollLeft += window.innerWidth -80;}}
+                >
+                    {">"}
+                </span>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Row
