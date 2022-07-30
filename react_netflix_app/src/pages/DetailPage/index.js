03f7bffb36f52ee21717578c7f14481eee3b29cc
chaud1024 <chaud1024@gmail.com>
2022-07-30 17:56:52 +0900

    detail page

--- react_netflix_app/src/pages/DetailPage/index.js
+++ react_netflix_app/src/pages/DetailPage/index.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from '../../api/axios';
+import { useParams } from 'react-router-dom'
 
 const DetailPage = () => {
+  const { movieId } = useParams();
+  //console.log('movieId', movieId);
+
+  const [movie, setMovie] = useState({})
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(
+        `/movie/${movieId}`
+      )
+      // console.log('request', request);
+      setMovie(request.data)
+    }
+    fetchData();
+  }, [movieId]); // everytime the movieId is changed, call the async function fetchData
+
+  if(!movie) return <div>해당 영화에 대한 상세 정보가 없습니다.</div>
+  
+
   return (
-    <div>DetailPage</div>
+    <section>
+      <img
+        className='modal__poster-img'
+        src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+        alt="poster"
+      />
+    </section>
   )
 }
 
